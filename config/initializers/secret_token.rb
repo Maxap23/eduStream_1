1ab41b614c34ac52eeb7fc5226a73df6b70e6c20
Maxime Paul <maxime.a.paul@gmail.com>
2013-08-08 22:43:33 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduStream1::Application.config.secret_token = 'f50c772b6299af7af8a785cb510050da6c6f8bc41172e13432c0d5a1707174e342a44b2c19eafc5f989157e088ba544fe79eacbfcca98a5edc6304c7a7a22441'
